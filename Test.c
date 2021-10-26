1dd9e10f4cff005d9c53b57ace6a5a121c1f01db
Anurag <anurag110049@gmail.com>
2021-10-26 14:35:32 +0530

    a

--- Test.c
+++ Test.c
@@ -1,41 +1,46 @@
+/* send_col_to_row.c -- send column 1 of a matrix on process 0 to row 1
+ *     on process 1.
+ *
+ * Input: none
+ * Output: The row received by process 1.
+ *
+ * Note:  This program should only be run with 2 processes
+ *
+ * See Chap 6., pp. 98 & ff in PPMPI
+ */
 #include <stdio.h>
-#include<stdlib.h>
-#include"mpi.h"
-int main(int argc, char **argv)
-{
-    MPI_Init(&argc,&argv);
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
-    int x,y,z;
-    MPI_Status status;
-    switch(world_rank)
-    {
-        case 0: x=0; y=1; z=2;
-                MPI_Bcast(&x,1,MPI_INT,0,MPI_COMM_WORLD);
-                MPI_Send(&y,1,MPI_INT,2,43,MPI_COMM_WORLD);
-                printf("After Send \n");
-                printf("z = %d\n",z);
-                MPI_Bcast(&z,1,MPI_INT,1,MPI_COMM_WORLD);
-                printf("z = %d\n",z);
-                // printf("Inside break\n");
-                // printf("world rank = %d x=%d, y=%d, z=%d\n",world_rank,x,y,z);
-                break;
-        
-        case 1: x=3; y=69; z=5;
-                MPI_Bcast(&x,1,MPI_INT,0,MPI_COMM_WORLD);
-                MPI_Bcast(&x,1,MPI_INT,1,MPI_COMM_WORLD);
-                // printf("Inside break\n");
-                // printf("world rank = %d x=%d, y=%d, z=%d\n",world_rank,x,y,z);
-                break;
-        
-        
+#include "mpi.h"
 
+int main(int argc, char* argv[]) {
+    int p;
+    int my_rank;
+    float A[10][10];
+    MPI_Status status;
+    MPI_Datatype column_mpi_t;
+    int i, j;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    MPI_Type_vector(10, 1, 10, MPI_FLOAT, &column_mpi_t);
+    MPI_Type_commit(&column_mpi_t);
+
+    if (my_rank == 0) {
+        for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
+                A[i][j] = (float) j;
+        MPI_Send(&(A[0][2]), 1, column_mpi_t, 1, 0,
+                 MPI_COMM_WORLD);
+    } else { /* my_rank = 1 */
+        for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
+                A[i][j] = 0.0;
+        MPI_Recv(A, 10, MPI_FLOAT, 0, 0,
+                 MPI_COMM_WORLD, &status);
+        for (j = 0; j < 10; j++)
+            printf("%3.1f ", A[0][j]);
+        printf("\n");
     }
-    if(world_rank<3)
-        printf("world rank = %d x=%d, y=%d, z=%d\n",world_rank,x,y,z);
 
     MPI_Finalize();
-    return 0;
-}
+}  /* main */
